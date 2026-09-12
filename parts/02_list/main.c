9a6f78bc4119717bf1e88d05a3198808d1e857af
ZeK-Harder <2455219615@qq.com>
2026-09-12 17:55:12 +0800

    feat:在main.c中通过delete_node的返回值来判断是否真正删除

--- parts/02_list/main.c
+++ parts/02_list/main.c
@@ -5,16 +5,32 @@ int main(){
     append_to_tail(&head, 2);
     append_to_tail(&head, 3);
     print_list(head);
-    delete_node(&head, 2);
+    if(delete_node(&head, 2)){
+        printf("已删除\n");
+    }else{
+        printf("未删除\n");
+    }
     printf("删除2后:\n");
     print_list(head);
-    delete_node(&head, 1);
+    if(delete_node(&head, 1)){
+        printf("已删除\n");
+    }else{
+        printf("未删除\n");
+    }   
     printf("删除1后:\n");
     print_list(head);
-    delete_node(&head, 99);
+    if(delete_node(&head, 99)){
+        printf("已删除\n");
+    }else{
+        printf("未删除\n");
+    }
     printf("删除99后:\n");
     print_list(head);
-    delete_node(&head, 3);
+    if(delete_node(&head, 3)){
+        printf("已删除\n");
+    }else{
+        printf("未删除\n");
+    }
     printf("删除3后:\n");
     print_list(head);
     list_free(head);
